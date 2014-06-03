ab3a676dc96ecfe05b0ed0351ac819843c4a6486
bryan <baj306@gmail.com>
2014-06-02 23:15:52 -0400

    Putting back in the temperature monitoring stuffs

--- garden.node/main.js
+++ garden.node/main.js
@@ -11,6 +11,13 @@ var node = process.argv[4];
 var serial = process.argv[5];
 var camera = process.argv[5];
 
+process.on('uncaughtException', function(e){
+  console.error("uncaught: " + e);
+  if(e.toString().indexOf("Disconnected") >= 0 || e.toString().indexOf("Opening ") >= 0){
+    serialError();
+  }
+})
+
 
 fs.readFile(process.argv[2], 'utf8', function (err,data) {
   if (err) {
@@ -42,105 +49,72 @@ fs.readFile(process.argv[2], 'utf8', function (err,data) {
   }
 
   takePicture();
-  });
 
+  var sp = null;
+  var reconnect = null;
 
-//var fs = require('fs');
-//var imageStream = fs.createWriteStream('capture.jpg');
-//imagesnap().pipe(imageStream);
+  function connect(){
+    console.log("Opening serial port on " + serial);
+    sp = new SerialPort(serial, {
+      baudrate: 9600,
+      parser: serialport.parsers.readline("\n")
+    });
+    sp.on("error", serialError);
+    sp.on("data", serialData);
+  }
 
-//var Camelot = require('camelot');
-//
-//var camelot = new Camelot( {
-//  'rotate' : '180',
-//  'flip' : 'v'
-//});
-//
-//camelot.on('frame', function (image) {
-//  console.log('frame received!');
-//});
-//
-//camelot.on('error', function (err) {
-//  console.log(err);
-//});
-//
-//camelot.grab( {
-//  'title' : 'Camelot',
-//  'font' : 'Arial:24',
-//  'frequency' : 1
-//});
+  function serialError(){
+    if(reconnect){
+      clearTimeout(reconnect);
+      reconnect = null;
+    }
+    console.log("Error on serial port. Reconnecting in 3 seconds...");
+    reconnect = setTimeout(function(){
+      connect()
+    },3000);
+  }
 
-//var sp = null;
-//var reconnect = null;
-//
-//process.on('uncaughtException', function(e){
-//  console.error("uncaught: " + e);
-//  if(e.toString().indexOf("Disconnected") >= 0 || e.toString().indexOf("Opening ") >= 0){
-//    serialError();
-//  }
-//})
-//
-//function connect(){
-//  console.log("Opening serial port on " + serial);
-//  sp = new SerialPort(serial, {
-//    baudrate: 9600,
-//    parser: serialport.parsers.readline("\n")
-//  });
-//  sp.on("error", serialError);
-//  sp.on("data", serialData);
-//}
-//
-//function serialError(){
-//  if(reconnect){
-//    clearTimeout(reconnect);
-//    reconnect = null;
-//  }
-//  console.log("Error on serial port. Reconnecting in 3 seconds...");
-//  reconnect = setTimeout(function(){
-//    connect()
-//  },3000);
-//}
-//
-//function serialData(data){
-//
-//  process.nextTick(function(){
-//    console.log("Parsing json...")
-//    var json = JSON.parse(data);
-//    console.log("Parsed!");
-//    json.node = node;
-//    try{
-//      // Build the post string from an object
-//      var post_data = querystring.stringify(json);
-//
-//      // An object of options to indicate where to post to
-//      var post_options = {
-//        host: server,
-//        port: port,
-//        path: '/rest/measurement',
-//        method: 'POST',
-//        headers: {
-//          'Content-Type': 'application/x-www-form-urlencoded',
-//          'Content-Length': post_data.length
-//        }
-//      };
-//
-//      // Set up the request
-//      var post_req = http.request(post_options, function(res) {
-//        res.setEncoding('utf8');
-//        res.on('data', function (chunk) {
-//          console.log('Response: ' + chunk);
-//        });
-//      });
-//
-//      // post the data
-//      post_req.write(post_data);
-//      post_req.end()
-//    }
-//    catch(e){
-//      console.error("Failed to post: " + e);
-//    }
-//  });
-//  console.log("here: "+data);
-//}
-//
-//connect();
+  function serialData(data){
+
+    process.nextTick(function(){
+      console.log("Parsing json...")
+      var json = JSON.parse(data);
+      console.log("Parsed!");
+      json.node = node;
+      try{
+        // Build the post string from an object
+        var post_data = querystring.stringify(json);
+
+        // An object of options to indicate where to post to
+        var post_options = {
+          host: server,
+          port: port,
+          path: '/rest/measurement',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+          }
+        };
+
+        // Set up the request
+        var post_req = http.request(post_options, function(res) {
+          res.setEncoding('utf8');
+          res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+          });
+        });
+
+        // post the data
+        post_req.write(post_data);
+        post_req.end()
+      }
+      catch(e){
+        console.error("Failed to post: " + e);
+      }
+    });
+    console.log("here: "+data);
+  }
+
+  connect();
+});
