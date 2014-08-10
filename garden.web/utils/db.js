49609edb1cde6debb47af3314e8e6070311e0fc2
bryan <baj306@gmail.com>
2014-08-10 00:04:46 -0400

    Trying out some averaging queries

--- garden.web/utils/db.js
+++ garden.web/utils/db.js
@@ -114,7 +114,7 @@ module.exports = {
   },
   latestMeasurementsByHour: function(callback){
     log.info("Querying measurements by endkey");
-    db.view("measurements/byHour", {slimit:1000, descending:true}, function(err, results){
+    db.view("measurements/byHour", {limit:1000, descending:true}, function(err, results){
       log.info("Results came back...")
       if(err){
         log.info("Failed to fetch latest measurements by key: " + JSON.stringify(err));
