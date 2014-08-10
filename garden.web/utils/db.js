d9a9bc19377382708a1719977ea3734be54f4b3b
bryan <baj306@gmail.com>
2014-08-09 23:52:51 -0400

    Trying out some averaging queries

--- garden.web/utils/db.js
+++ garden.web/utils/db.js
@@ -47,18 +47,18 @@ db.save('_design/measurements', {
         // value you want averaged here
       },
       // reduce
-      reduce: function(keys, values, rereduce) {
-        if (!rereduce){
-          var length = values.length
-          return [sum(values) / length, length]
-        }else{
-          var length = sum(values.map(function(v){return v[1]}))
-          var avg = sum(values.map(function(v){
-            return v[0] * (v[1] / length)
-          }))
-          return [avg, length]
-        }
-      }
+//      reduce: function(keys, values, rereduce) {
+//        if (!rereduce){
+//          var length = values.length
+//          return [sum(values) / length, length]
+//        }else{
+//          var length = sum(values.map(function(v){return v[1]}))
+//          var avg = sum(values.map(function(v){
+//            return v[0] * (v[1] / length)
+//          }))
+//          return [avg, length]
+//        }
+//      }
 //      map: function (doc) {
 //        if (doc.type === 'measurement') {
 //          emit(Math.floor(doc.time/1000), parseFloat(doc.moisture));
